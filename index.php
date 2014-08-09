7261efb6a52060a919a1b8dcb28560e4db652d9f
algor <o.gorbachevskyi@gmail.com>
2014-08-09 18:42:13 +0300

    [+] Added basic html

--- index.php
+++ index.php
@@ -8,73 +8,28 @@ if ((float)PCRE_VERSION<7.9)
 
 $f3->config('config.ini');
 
-$f3->route('GET /',
-	function($f3) {
-		$classes=array(
-			'Base'=>
-				array(
-					'hash',
-					'json',
-					'session'
-				),
-			'Cache'=>
-				array(
-					'apc',
-					'memcache',
-					'wincache',
-					'xcache'
-				),
-			'DB\SQL'=>
-				array(
-					'pdo',
-					'pdo_dblib',
-					'pdo_mssql',
-					'pdo_mysql',
-					'pdo_odbc',
-					'pdo_pgsql',
-					'pdo_sqlite',
-					'pdo_sqlsrv'
-				),
-			'DB\Jig'=>
-				array('json'),
-			'DB\Mongo'=>
-				array(
-					'json',
-					'mongo'
-				),
-			'Auth'=>
-				array('ldap','pdo'),
-			'Bcrypt'=>
-				array(
-					'mcrypt',
-					'openssl'
-				),
-			'Image'=>
-				array('gd'),
-			'Lexicon'=>
-				array('iconv'),
-			'SMTP'=>
-				array('openssl'),
-			'Web'=>
-				array('curl','openssl','simplexml'),
-			'Web\Geo'=>
-				array('geoip','json'),
-			'Web\OpenID'=>
-				array('json','simplexml'),
-			'Web\Pingback'=>
-				array('dom','xmlrpc')
-		);
-		$f3->set('classes',$classes);
-		$f3->set('content','welcome.htm');
-		echo View::instance()->render('layout.htm');
-	}
-);
 
-$f3->route('GET /userref',
-	function($f3) {
-		$f3->set('content','userref.htm');
-		echo View::instance()->render('layout.htm');
-	}
+// routes
+
+// Routing
+//$f3->route('POST /@controller/@action [ajax]','@controller->@action');
+//$f3->route('POST /@controller/@action','@controller->@action');
+
+$f3->route('GET /','main->index');
+$f3->route('GET /products','main->products');
+$f3->route('GET /about','main->about');
+$f3->route('GET /contacts','main->contacts');
+$f3->route('GET /delivery','main->delivery');
+
+//$f3->route('POST /flats','main->flats');
+//$f3->route('POST /admin','admin->index');
+
+
+$f3->set('ONERROR',
+    function($f3) {
+        echo 'Errorrrr';
+        var_dump($f3->get('ERROR.text'));
+    }
 );
 
 $f3->run();
---
+++ lib/main.php
@@ -0,0 +1,38 @@
+<?php
+
+class Main {
+    function index($f3) {
+
+        echo View::instance()->render('header.html');
+        echo View::instance()->render('footer.html');
+
+    }
+
+    function products($f3) {
+
+        echo View::instance()->render('header.html');
+        echo View::instance()->render('footer.html');
+
+    }
+
+    function about($f3) {
+
+        echo View::instance()->render('header.html');
+        echo View::instance()->render('footer.html');
+
+    }
+
+    function contacts($f3) {
+
+        echo View::instance()->render('header.html');
+        echo View::instance()->render('footer.html');
+
+    }
+
+    function delivery($f3) {
+
+        echo View::instance()->render('header.html');
+        echo View::instance()->render('footer.html');
+
+    }
+}
